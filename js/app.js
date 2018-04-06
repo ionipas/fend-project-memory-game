dc4800bbbab57fe5aeaffb44a2ae32b714f5638f
Ionela Pastrama <ionipas@yahoo.com>
2018-04-06 23:59:57 +0300

    Replace "var" with "let"

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ function resetStar() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
